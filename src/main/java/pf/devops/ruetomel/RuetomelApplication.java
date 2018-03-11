88ae0f2fbe413b2d0541970e01108f04d85a8461
Peter MEUEL <peter@teriiehina.net>
2018-03-10 22:47:01 -1000

    Import template from Spring Initializr

---
+++ src/main/java/pf/devops/ruetomel/RuetomelApplication.java
@@ -0,0 +1,12 @@
+package pf.devops.ruetomel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RuetomelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RuetomelApplication.class, args);
+	}
+}
