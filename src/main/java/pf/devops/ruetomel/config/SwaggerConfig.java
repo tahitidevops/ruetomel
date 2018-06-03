61b719f3a1b058d80b17daa3cdf8945fbf8ca66c
Peter MEUEL <pmeuel@cps.pf>
2018-06-02 14:31:35 -1000

    Add swagger

---
+++ src/main/java/pf/devops/ruetomel/config/SwaggerConfig.java
@@ -0,0 +1,36 @@
+package pf.devops.ruetomel.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import static springfox.documentation.builders.PathSelectors.regex;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+public class SwaggerConfig {
+
+  @Bean
+  public Docket productApi() {
+    return new Docket(DocumentationType.SWAGGER_2)
+        .select()
+        .apis(RequestHandlerSelectors.basePackage("pf.devops.ruetomel"))
+        .paths(regex("/*"))
+        .build()
+        .apiInfo(metaData());
+
+  }
+
+  private ApiInfo metaData() {
+    return new ApiInfoBuilder().title("API de Pepa")
+        .description("Récupération de documents")
+        .version("1.0.0")
+        .license("Apache License Version 2.0")
+        .licenseUrl("https://www.apache.org/licenses/LICENSE-2.0\"")
+        .contact(new Contact("Peter MEUEL", null, "peter@teriiehina.pf"))
+        .build();
+  }
+}
