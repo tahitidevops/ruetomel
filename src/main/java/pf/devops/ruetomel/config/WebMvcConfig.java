5e7bc0adea182103c595eaf40f767d60da74d5f9
Peter MEUEL <pmeuel@cps.pf>
2018-06-02 14:28:32 -1000

    Change inclusion of Bootstrap to webjars

--- src/main/java/pf/devops/ruetomel/config/WebMvcConfig.java
+++ src/main/java/pf/devops/ruetomel/config/WebMvcConfig.java
@@ -1,8 +1,12 @@
 package pf.devops.ruetomel.config;
 
+import java.util.Locale;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 @Configuration
 public class WebMvcConfig //extends WebMvcConfigurationSupport
@@ -11,5 +15,13 @@ public class WebMvcConfig //extends WebMvcConfigurationSupport
     protected void addInterceptors(InterceptorRegistry registry)
     {
 //        registry.addInterceptor(new ThymeleafLayoutInterceptor());
+    }
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        SessionLocaleResolver resolver = new SessionLocaleResolver();
+        resolver.setDefaultLocale(Locale.FRENCH);
+
+        return resolver;
     }
 }
